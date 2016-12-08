4547832997c180a824d9d4eaef4a5c5bea9edb31
Suvan <suvanramani@gmail.com>
2016-12-08 12:16:01 -0800

    Rotate plus button back and forth

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -23,9 +23,28 @@ class ViewController: UIViewController {
     // MARK: Actions
 
     @IBAction func plusButton(_ sender: UIButton) {
+        if navBarHeight.constant == 64 {
+            expandNavBar()
+        } else if navBarHeight.constant == 200 {
+            collapseNavBar()
+        }
+    }
+    
+    
+    func expandNavBar() {
         UIView.animate(withDuration: 2.0, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: .beginFromCurrentState, animations: {() -> Void in
-                self.navBarHeight.constant = 200
-                self.view.layoutIfNeeded()
+            self.navBarHeight.constant = 200
+            self.plusButton.transform = CGAffineTransform(rotationAngle: 95)
+            self.view.layoutIfNeeded()
+        }, completion: { finished in
+        })
+    }
+    
+    func collapseNavBar() {
+        UIView.animate(withDuration: 2.0, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .beginFromCurrentState, animations: {() -> Void in
+            self.navBarHeight.constant = 64
+            self.plusButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            self.view.layoutIfNeeded()
         }, completion: { finished in
         })
     }
